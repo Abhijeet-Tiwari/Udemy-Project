c3e648f48489e3126ab6ad72d09f160344acf34f
Abhijeet Tiwari <abhijeet96tiwari@gmail.com>
2018-07-02 09:15:37 +0530

    Creating and Nesting of Components

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { StylesCompileDependency } from '@angular/compiler';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  // line Styling
+//   styles:[`
+//   h3 {
+//     color: dodgerblue;
+//   }`
+// ]
+})
+export class AppComponent {
+  
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // Different ways to use selector
+  // 1. selecting the component by using selector as an element (see how to use this as element in the html).
+  selector: 'app-servers',
+  // 2. selecting the component by using selector as an attribute (see how to use this as template in the html).
+  //  selector: '[app-servers]',
+  // 3. selecting the component by using selector as an class (see how to use this as class in the html).
+  // selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
